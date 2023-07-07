4080e0d9f67fa2b96d2fa6a8256943c1b400b457
LAPTOP-4V8ET9Q2\irisr <irisrn2004@gmail.com>
2023-07-07 12:50:31 +0200

    fixed text and line

--- pages/index.vue
+++ pages/index.vue
@@ -13,11 +13,50 @@ export default {
 }
 </script>
 <style>
-.guitar-banner {}
+.guitar-banner {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    overflow: hidden;
+    margin-top: -10px;
+}
 
-.banner-img {}
+.banner-img {
+    width: 100%;
+}
 
-.banner-overlay {}
+.banner-overlay {
+    position: absolute;
+    top: 0;
+    -webkit-box-shadow: inset 0px -22px 50px -5px rgba(0, 0, 0, 0.43);
+    box-shadow: inset 0px -22px 50px -5px rgba(0, 0, 0, 0.43);
+    width: 100%;
+    height: 100%;
+}
 
-.banner-text {}
+.banner-text {
+    position: absolute;
+    top: 50%;
+    border-left: 8px solid var(--red);
+    padding-left: 20px;
+    font-size: 70px;
+    color: var(--white);
+    font-family: var(--headingfont);
+    font-style: italic;
+    font-weight: var(--headingfontweight);
+    margin-left: 50px;
+    text-shadow: 2px 2px 8px var(--black);
+}
+
+/* -------------------------------------------- media queries */
+@media screen and (max-width: 896px) {}
+
+@media screen and (max-width: 768px) {
+    .banner-img {
+        height: 100%;
+        margin-left: -689px;
+    }
+}
 </style>
