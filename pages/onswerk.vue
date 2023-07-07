6dfde4c98709179930d844f091896dc55685f5ee
LAPTOP-4V8ET9Q2\irisr <irisrn2004@gmail.com>
2023-07-07 16:57:57 +0200

    added component for page title

---
+++ pages/onswerk.vue
@@ -0,0 +1,12 @@
+<template>
+    <Navbar></Navbar>
+    <Pagetitle pageTitle="Ons werk"></Pagetitle>
+</template>
+<script>
+import Navbar from '/././components/Navbar.vue';
+import Pagetitle from '/././components/Pagetitle.vue';
+export default {
+
+}
+</script>
+<style scoped></style>
