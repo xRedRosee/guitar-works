77670ed9b1dfefeff516308456c919864f831aae
LAPTOP-4V8ET9Q2\irisr <irisrn2004@gmail.com>
2023-07-07 12:33:49 +0200

    set up html for homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
     <Navbar></Navbar>
+    <section class="guitar-banner">
+        <img src="../assets/banner.JPG" alt="guitar banner" class="banner-img">
+        <span class="banner-overlay"></span>
+        <h1 class="banner-text"> Guitarworks <br> Guitar Repair & Customizing Panningen</h1>
+    </section>
 </template>
 <script>
 import Navbar from '/././components/Navbar.vue';
@@ -7,4 +12,12 @@ export default {
 
 }
 </script>
-<style></style>
+<style>
+.guitar-banner {}
+
+.banner-img {}
+
+.banner-overlay {}
+
+.banner-text {}
+</style>
