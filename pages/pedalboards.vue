39b686ccc092689142ad5e57a7762f49b5adc28d
LAPTOP-4V8ET9Q2\irisr <irisrn2004@gmail.com>
2023-08-19 12:54:24 +0200

    added slideshow

--- pages/pedalboards.vue
+++ pages/pedalboards.vue
@@ -26,10 +26,75 @@
             </div>
         </div>
     </section>
+    <section class="beeld-section">
+        <Pagetitle pageTitle="Pedalboards in beeld"></Pagetitle>
+        <p class="beeld-intro"> Deze foto’s geven je een impressie van wat er mogelijk is… </p>
+        <div class="glide slide-div beeld-slides">
+            <div class="glide__track" data-glide-el="track">
+                <ul class="glide__slides">
+                    <li class="glide__slide show-slide">
+                        <img src="../assets/Pedalboards/pedalboard1.jpg" alt="pedal board" class="show-img">
+                    </li>
+                    <li class="glide__slide show-slide">
+                        <img src="../assets/Pedalboards/pedalboard2.jpg" alt="pedal board" class="show-img">
+                    </li>
+                    <li class="glide__slide show-slide">
+                        <img src="../assets/Pedalboards/pedalboard3.jpg" alt="pedal board" class="show-img">
+                    </li>
+                    <li class="glide__slide show-slide">
+                        <img src="../assets/Pedalboards/pedalboard4.jpg" alt="pedal board" class="show-img">
+                    </li>
+                    <li class="glide__slide show-slide">
+                        <img src="../assets/Pedalboards/pedalboard5.jpg" alt="pedal board" class="show-img">
+                    </li>
+                    <li class="glide__slide show-slide">
+                        <img src="../assets/Pedalboards/pedalboard6.jpeg" alt="pedal board" class="show-img">
+                    </li>
+                    <li class="glide__slide show-slide">
+                        <img src="../assets/Pedalboards/pedalboard7.jpeg" alt="pedal board" class="show-img">
+                    </li>
+                    <li class="glide__slide show-slide">
+                        <img src="../assets/Pedalboards/pedalboard8.jpeg" alt="pedal board" class="show-img">
+                    </li>
+                    <li class="glide__slide show-slide">
+                        <img src="../assets/Pedalboards/pedalboard9.JPG" alt="pedal board" class="show-img">
+                    </li>
+                    <li class="glide__slide show-slide">
+                        <img src="../assets/Pedalboards/Multi Snake op maat....JPG" alt="pedal board" class="show-img">
+                    </li>
+                </ul>
+            </div>
+            <div class="glide__controls buttons" data-glide-el="controls">
+                <button class="glide__arrow glide__arrow--left button-left" data-glide-dir="|<">
+                    <img alt="arrow left" class="arrow-img" src="../assets/arrow-left-red.svg">
+                </button>
+                <button class="glide__arrow glide__arrow--right button-right" data-glide-dir="|>">
+                    <img alt="arrow right" class="arrow-img" src="../assets/arrow-right-red.svg">
+                </button>
+            </div>
+        </div>
+    </section>
 </template>
 <script>
+import Glide from '@glidejs/glide'
+import Navbar from '/././components/Navbar.vue';
+import Pagetitle from '/././components/Pagetitle.vue';
 export default {
-
+    mounted() {
+        new Glide('.glide', {
+            type: 'carousel',
+            animationDuration: 2000,
+            // go to next slide after 7 seconds
+            autoplay: 7000,
+            // show 1 slide, starting with first slide
+            focusAt: '1',
+            startAt: 1,
+            perView: 1,
+            gap: 0,
+        }).mount()
+    },
 }
 </script>
-<style></style>
+<style>
+@import '@glidejs/glide/dist/css/glide.core.min.css';
+</style>
