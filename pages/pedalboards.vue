fbdf05708adb23aa18d1cff1b578d5ebd879f48e
LAPTOP-4V8ET9Q2\irisr <irisrn2004@gmail.com>
2023-08-19 12:44:29 +0200

    added pedal boards page intro header

---
+++ pages/pedalboards.vue
@@ -0,0 +1,35 @@
+<template>
+    <Navbar></Navbar>
+    <section class="top-header">
+        <Pagetitle pageTitle="Pedalboards"></Pagetitle>
+        <div class="header-info">
+            <div class="text-side">
+                <article class="header-intro-text">
+                    <p class="header-text">Als gitarist zijnde begeef je je vroeg of laat in de wereld van gitaareffecten.
+                        Naarmate dat de hoeveelheid effecten (meestal in pedaalvorm) zich uitbreiden, ontstaan ook de eerste
+                        problemen.
+                    </p>
+                    <p class="header-text"> - Welke volgorde van effecten moet ik hanteren? <br>
+                        - Wat voor patch (verbindings) kabeltjes kan ik het beste gebruiken? <br>
+                        - Welke voeding kan ik het beste gebruiken? <br>
+                        Ik wil alles graag idiot proof een road-ready gemonteerd hebben… Kan dat?
+                    </p>
+                    <p class="header-text"> Ja, wij bij Guitarworks hebben al bijna 100 pedalboard gebouwd om er voor te
+                        zorgen dat zowel de hobby-gitarist alsook de professionele-gitarist probleemloos met zijn
+                        ‘’effectenbundel’’ kan spelen zonder last te hebben van ruis-, brom-, kabel- of voedingsproblemen.
+                        Wij maken een pedalboard op maat met desgewenst ook een multisnake (=kabelbundel) op maat.
+                    </p>
+                </article>
+            </div>
+            <div class="img-side">
+                <img class="header-img" alt="pedalboard foto" src="../assets/pedalboards.JPG">
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style></style>
